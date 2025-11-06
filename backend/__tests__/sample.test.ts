fb511de3319d78beecac0fcbfbe5e9dc93476e26
unknown <RS-EMP@PC-YAMINEESH.secure.rrootshell.com>
2025-11-06 16:54:07 +0530

    test(backend): update Jest config to use JS tests instead of TS

--- backend/__tests__/sample.test.ts
+++
@@ -1,3 +0,0 @@
-test('sample sanity test', () => {
-  expect(1 + 1).toBe(2);
-});
--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
 };
